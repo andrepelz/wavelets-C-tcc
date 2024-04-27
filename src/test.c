e51568b05e0f37a9aab6b37e0966bbc9549f0f33
Andre <andre.valim@edu.univali.br>
2024-04-26 21:40:44 -0300

    feat: Adiciona lógica para suportar sinais de tamanho não equivalente N*2^nivel

--- src/test.c
+++ src/test.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 #include "wavelets.h"
 
 #define ARRAY_SIZE 16
-#define ALTERNATE_ARRAY_SIZE 24
+#define ALTERNATE_ARRAY_SIZE 25
 
 void reference_test() {
     double base_arr[ARRAY_SIZE] = { 32, 10, 20, 38, 37, 28, 38, 34, 18, 24, 18, 9, 23, 24, 28, 34 };
@@ -42,17 +43,36 @@ void alternate_test() {
 
     uint16_t depth = 3;
 
-    transform = haar_transform(base_arr, ALTERNATE_ARRAY_SIZE, depth);
+    uint16_t depth_mod_test = pow(2, depth); // padding added to meet condition of input size being N*2^depth
+    uint16_t padding = ALTERNATE_ARRAY_SIZE%depth_mod_test != 0
+        ? depth_mod_test - ALTERNATE_ARRAY_SIZE%depth_mod_test
+        : 0;
+
+    uint16_t arr_size = ALTERNATE_ARRAY_SIZE + padding;
+    double *arr_ptr = (double *)malloc(
+        arr_size*sizeof(double));
+
+    for(int i = 0; i < arr_size; i++)
+        arr_ptr[i] = i < ALTERNATE_ARRAY_SIZE ? base_arr[i] : 0;
+
+    end = &arr_ptr[arr_size];
+
+    for(iter = arr_ptr; iter != end; iter++)
+        printf("%.2lf\t", *iter);
+
+    printf("\n\n");
+    
+    transform = haar_transform(arr_ptr, arr_size, depth);
 
     for(iter = transform; iter != end; iter++)
-        printf("%lf\t", *iter);
+        printf("%.2lf\t", *iter);
 
     printf("\n\n");
 
-    inverse_transform = haar_inverse_transform(base_arr, ALTERNATE_ARRAY_SIZE, depth);
+    inverse_transform = haar_inverse_transform(transform, arr_size, depth);
 
     for(iter = inverse_transform; iter != end; iter++)
-        printf("%lf\t", *iter);
+        printf("%.2lf\t", *iter);
 
     printf("\n\n");
 }
