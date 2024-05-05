671e07243359e07540ddd19ff56118bb363fb57a
Andre <andre.valim@edu.univali.br>
2024-05-05 17:13:29 -0300

    fix: Adiciona assinatura de calculate_threshold para utils.h

--- headers/utils.h
+++ headers/utils.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 
+double calculate_threshold(double* d1, size_t original_size, double k, double m);
+
 double snr(double* signal, double* noise, size_t size);
 double mse(double* original_signal, double* resulting_signal, size_t size);
 
