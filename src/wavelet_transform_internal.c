5e61ec18dc10c59e6ae8414a56c0e6662c9750a6
André Valim <andre.valim@edu.univali.br>
2024-06-09 14:23:12 -0300

    refactor: Remove paralelismo de dwt_1L e idwt_1L

--- src/wavelet_transform_internal.c
+++ src/wavelet_transform_internal.c
@@ -8,24 +8,17 @@ void dwt_1L(double *approx_in, double *approx_out, double *detail_out, size_t si
     }
 
     int t, j, l;
-    Wavelet wavelet_cpy;
 
-    #pragma omp parallel private(wavelet_cpy, j, l) shared(approx_in, approx_out, detail_out)
-    {
-        wavelet_cpy = wavelet;
+    for(t = 0; t < size/2; t++) {
+        j = 2*t + 1;
 
-        #pragma omp for
-        for(t = 0; t < size/2; t++) {
-            j = 2*t + 1;
+        for(l = 0; l < wavelet.filter_size; l++) {
+            if(j < 0) j = size - 1;
 
-            for(l = 0; l < wavelet_cpy.filter_size; l++) {
-                if(j < 0) j = size - 1;
+            approx_out[t] += wavelet.scaling_filter[l]*approx_in[j];
+            detail_out[t] += wavelet.wavelet_filter[l]*approx_in[j];
 
-                approx_out[t] += wavelet_cpy.scaling_filter[l]*approx_in[j];
-                detail_out[t] += wavelet_cpy.wavelet_filter[l]*approx_in[j];
-
-                j--;
-            }
+            j--;
         }
     }
 }
@@ -36,29 +29,22 @@ void idwt_1L(double *approx_in, double *detail_in, double *approx_out, size_t si
     }
 
     int t, j, l;
-    Wavelet wavelet_cpy;
 
-    #pragma omp parallel private(wavelet_cpy, j, l) shared(approx_in, detail_in, approx_out)
-    {
-        wavelet_cpy = wavelet;
+    for(t = 0; t < size/2; t++) {
+        j = t;
 
-        #pragma omp for
-        for(t = 0; t < size/2; t++) {
-            j = t;
+        for(l = 0; l < wavelet.filter_size; l += 2) {
+            j = j%(size/2);
 
-            for(l = 0; l < wavelet_cpy.filter_size; l += 2) {
-                j = j%(size/2);
+            double even_approx_temp = wavelet.scaling_filter[l + 1]*approx_in[j];
+            double even_detail_temp = wavelet.wavelet_filter[l + 1]*detail_in[j];
+            double odd_approx_temp = wavelet.scaling_filter[l]*approx_in[j];
+            double odd_detail_temp = wavelet.wavelet_filter[l]*detail_in[j];
 
-                double even_approx_temp = wavelet_cpy.scaling_filter[l + 1]*approx_in[j];
-                double even_detail_temp = wavelet_cpy.wavelet_filter[l + 1]*detail_in[j];
-                double odd_approx_temp = wavelet_cpy.scaling_filter[l]*approx_in[j];
-                double odd_detail_temp = wavelet_cpy.wavelet_filter[l]*detail_in[j];
+            approx_out[2*t] += even_approx_temp + even_detail_temp;
+            approx_out[2*t + 1] += odd_approx_temp + odd_detail_temp;
 
-                approx_out[2*t] += even_approx_temp + even_detail_temp;
-                approx_out[2*t + 1] += odd_approx_temp + odd_detail_temp;
-
-                j++;
-            }
+            j++;
         }
     }
 }
